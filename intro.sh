34135a6cd94101604eaa7610faa286c17b79ea04
Drew Warner <koolzo@elec.tricity.wsu.edu>
2017-09-07 22:09:35 -0700

    Drew's HW1 commit.

--- intro.sh
+++ intro.sh
@@ -2,9 +2,10 @@
 
 echo "Hello, let me introduce myself"
 
-NAME=Michael
-HOMETOWN=Indianapolis
+NAME=Drew
+HOMETOWN=Kennewick
 MAJOR=CS
-FAVORITE_HOBBY=read
+FAVORITE_HOBBY=Speedrun
+FAVORITE_HOBBY_2=RPGs
 
-echo "My name is $NAME. I am originally from $HOMETOWN. My major is $MAJOR and in my free time I like to $FAVORITE_HOBBY"
+echo "My name is $NAME. I am originally from $HOMETOWN. My major is $MAJOR and in my free time I like to $FAVORITE_HOBBY $FAVORITE_HOBBY_2 (Read: Waste My Time). On an unrelated note, it sure is smoky out late, isn't it?"
